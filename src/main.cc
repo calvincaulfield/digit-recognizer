8abb09e3378cd707bfda8de10ec4cf811584e97d
Calvin Caulfield <calvin.caulfield@gmail.com>
2019-11-16 04:26:29 +0000

    Backed working

---
+++ src/main.cc
@@ -0,0 +1,18 @@
+#include "handwritten_digit_analyzer.h"
+
+int main() {
+	double learningRate = 10;
+
+	digit_recognizer::handwritten_digits_analyzer analyzer({ 100, 30 }, learningRate);
+
+	size_t numEpochs = 50;
+
+	analyzer.trainWithBinaryFile(numEpochs, "../../data/mnist/train-images.idx3-ubyte",
+		"../../data/mnist/train-labels.idx1-ubyte",
+		"../../data/mnist/t10k-images.idx3-ubyte",
+		"../../data/mnist/t10k-labels.idx1-ubyte");
+
+	//analyzer.trainWithTextFile(numEpochs, "../../../data/mnist/mnist_train.csv",
+		//"../../../data/mnist/mnist_test.csv");
+
+}
